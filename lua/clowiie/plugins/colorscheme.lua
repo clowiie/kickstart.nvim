4b37bcab586dcbaca2de3074ad6539a1eb28d806
clowiie <nakapol@lmwn.com>
2025-03-05 11:34:35 +0700

    perf: load tokyonight.nvim in the intended way

--- lua/clowiie/plugins/colorscheme.lua
+++ lua/clowiie/plugins/colorscheme.lua
@@ -5,13 +5,17 @@ return { -- You can easily change to a different colorscheme.
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   'dracula/vim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
+  config = function()
+    -- ---@diagnostic disable-next-line: missing-fields
+    -- require('dracula/vim').setup {
+    --   styles = {
+    --     comments = { italic = false }, -- Disable italics in comments
+    --   },
+    -- }
+
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.cmd.colorscheme 'dracula'
-
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
   end,
 }
