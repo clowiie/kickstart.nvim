8751a38395427f270f1d40cdc1b3192094202c75
clowiie <nakapol@lmwn.com>
2026-02-20 13:15:05 +0700

    fix: call setup on guess indent

--- lua/clowiie/plugins/guess-indent.lua
+++ lua/clowiie/plugins/guess-indent.lua
@@ -1,3 +1,4 @@
 return {
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  -- NOTE: Plugins can be added via a link or github org/name. To run setup automatically, use `opts = {}`
+  { 'NMAC427/guess-indent.nvim', opts = {} },
 }
